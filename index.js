009fcb3cc4de4436d2957b8b93feed7336f56e7a
Chris Broski <chrisbroski@gmail.com>
2017-06-29 20:40:01 +0000

    added picam support

--- index.js
+++ index.js
@@ -11,8 +11,7 @@ var port = 3789;
 var spawn = require('child_process').spawn;
 var fs = require("fs");
 var frogeye = require("./frogeye.js");
-//var Senses = require('./Senses.js');
-//var senses = new Senses(64, 48, !!process.argv[2]);
+
 var visionWidth = 64;
 var visionHeight = 48;
 var partialImgData = '';
@@ -54,6 +53,21 @@ function vision(yuvData, imgRawFileSize, imgPixelSize) {
     return lumaData;
 }
 
+function sendData(data, imgRawFileSize, imgPixelSize) {
+    var arrayData = vision(data, imgRawFileSize, imgPixelSize);
+
+    if (arrayData) {
+        var feData = frogeye(arrayData, imgPixelSize, visionWidth, 20);
+
+        io.emit('senseState', JSON.stringify({
+            dimensions: [64, 48],
+            motionLocation: feData.moveLocation,
+            brightnessOverall: feData.brightness,
+            edges: feData.edges
+        }));
+    }
+}
+
 function virt(i, imgRawFileSize, imgPixelSize) {
     var imgNum = parseInt(i / 10, 10) % 2 + 1;
     i += 1;
@@ -64,21 +78,48 @@ function virt(i, imgRawFileSize, imgPixelSize) {
             throw err;
         }
 
-        feData = frogeye(vision(data, imgRawFileSize, imgPixelSize), imgPixelSize, visionWidth, 20);
-        io.emit('senseState', JSON.stringify({
-            dimensions: [64, 48],
-            motionLocation: feData.moveLocation,
-            brightnessOverall: feData.brightness,
-            edges: feData.edges
-        }));
+        sendData(data, imgRawFileSize, imgPixelSize)
 
         virt_timer = setTimeout(function () {virt(i, imgRawFileSize, imgPixelSize); }, 250);
     });
 }
 
+function raspiCam(imgRawFileSize, imgPixelSize) {
+    var cam = spawn('raspiyuv', [
+        '-w', visionWidth.toString(10),
+        '-h', visionHeight.toString(10),
+        '--nopreview',
+        '-bm', // Burst mode
+        '-vf', // My camera is upside-down so flip the image vertically
+        '-hf',
+        '-tl', '0', // 0 = as fast as possible
+        '-t', '300000', // Restart every 5 min
+        '-o', '-' // To stdout
+    ]);
+
+    cam.stdout.on('data', function (data) {
+        sendData(data, imgRawFileSize, imgPixelSize);
+    });
+
+    cam.stderr.on('data', function (data) {
+        console.log('stderr: ' + data);
+    });
+
+    cam.on('exit', function (code) {
+        console.log('raspiyuv process exited with code ' + code);
+        console.log('Restarting raspiyuv time lapse');
+        raspiCam(visionWidth * visionHeight * 1.5, visionWidth * visionHeight);
+    });
+}
+
 io.on('connection', function (socket) {
     console.log('Frogeye viewer client connected');
-    virt(1, visionWidth * visionHeight * 1.5, visionWidth * visionHeight);
+
+    if (!!process.argv[2]) {
+        virt(1, visionWidth * visionHeight * 1.5, visionWidth * visionHeight);
+    } else {
+        raspiCam(visionWidth * visionHeight * 1.5, visionWidth * visionHeight);
+    }
 
     socket.on('disconnect', function () {
         if (virt_timer) {
